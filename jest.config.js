98e6fca20dc08054ddc09189a2b1081f3a995d41
Glavin Wiechert <glavin.wiechert@gmail.com>
2018-07-05 04:20:04 -0300

    Add Jest config

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+"use strict";
+
+module.exports = {
+  rootDir: ".",
+  testEnvironment: "node",
+  setupFiles: [],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testRegex: "test/.+\\.(test|spec)\\.ts$",
+  testPathIgnorePatterns: [
+    "<rootDir>/__mocks__/",
+    "<rootDir>/dist/",
+    "<rootDir>/node_modules/",
+  ],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  collectCoverage: true,
+  coverageReporters: ["json", "lcov", "text", "html"],
+  coveragePathIgnorePatterns: [
+    "<rootDir>/__mocks__/",
+    "<rootDir>/dist/",
+    "<rootDir>/test/",
+    "<rootDir>/node_modules/",
+  ],
+  globals: {
+    "ts-jest": {
+      tsConfigFile: "tsconfig.json",
+      enableTsDiagnostics: true,
+    },
+  },
+};
