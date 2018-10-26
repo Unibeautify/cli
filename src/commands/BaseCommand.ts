a196f834665ed2116055811fa099b65c2fb515e0
stevenzeck <steven.zeck@gmail.com>
2018-10-26 16:17:51 -0500

    Change to stderr

--- src/commands/BaseCommand.ts
+++ src/commands/BaseCommand.ts
@@ -13,7 +13,7 @@ export abstract class BaseCommand {
   }
 
   protected writeError(text: string): void {
-    this.stderr.write((this.stdout.isTTY ? chalk.red(text) : text) + "\n");
+    this.stderr.write((this.stderr.isTTY ? chalk.red(text) : text) + "\n");
   }
 
   protected set exitCode(exitCode: number) {
