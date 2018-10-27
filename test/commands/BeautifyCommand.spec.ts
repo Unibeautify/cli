dcf9d40bbbbe295e969d793a27760f276679aa8d
stevenzeck <steven.zeck@gmail.com>
2018-10-27 13:00:26 -0500

    Remove error for no text in file or stdin

--- test/commands/BeautifyCommand.spec.ts
+++ test/commands/BeautifyCommand.spec.ts
@@ -74,6 +74,22 @@ describe("BeautifyCommand", () => {
           expect(json.stdout).toBe('const test = "test";\n');
         });
     });
+    test("should accept blank files or text", async () => {
+      const command = new CustomCommand();
+      return command
+        .beautify({
+          args: [],
+          configFile: "test/.unibeautifyrc.yml",
+          language: "JavaScript",
+        })
+        .then(() => {
+          const json = command.toJSON();
+          expect(json.exitCode).toBe(0);
+          expect(json.stderr).toBe("");
+          // tslint:disable-next-line:quotemark
+          expect(json.stdout).toBe("\n");
+        });
+    });
     test("should beautify and write to file", async () => {
       const command = new CustomCommand("const test = 'test';");
       const originPath = "test/fixtures/test1.js";
@@ -195,32 +211,6 @@ describe("BeautifyCommand", () => {
           expect(catchCb).toHaveProperty(
             ["mock", "calls", 0, 0, "message"],
             "Cannot find language."
-          );
-          const json = command.toJSON();
-          expect(json.exitCode).toBe(1);
-          expect(json).toMatchSnapshot("json");
-        });
-    });
-    test("should throw error saying no text in file or stdin", async () => {
-      const command = new CustomCommand("");
-      const thenCb = jest.fn();
-      const catchCb = jest.fn();
-      return command
-        .beautify({
-          args: [],
-          configFile: "test/.unibeautifyrc.yml",
-          language: "JavaScript",
-        })
-        .then(thenCb)
-        .catch(catchCb)
-        .then(() => {
-          expect(thenCb).not.toBeCalled();
-          expect(catchCb).toHaveBeenCalled();
-          expect(catchCb.mock.calls).toHaveLength(1);
-          expect(catchCb.mock.calls[0]).toHaveLength(1);
-          expect(catchCb).toHaveProperty(
-            ["mock", "calls", 0, 0, "message"],
-            "No text in the file or stdin."
           );
           const json = command.toJSON();
           expect(json.exitCode).toBe(1);
